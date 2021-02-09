75d2bac53d428572114ffd23e0074b9e8ababf8f
Ibrahim Shorif <76436994+ibrahimshorif@users.noreply.github.com>
2021-02-09 19:23:42 +0600

    DataPassing

---
+++ app/src/main/java/com/example/hostalapp/testTab/ulits/DataPassing.java
@@ -0,0 +1,10 @@
+
+package com.example.hostelapp.testTab.utils;
+
+import java.util.LinkedHashMap;u
+import java.util.List;
+
+public interface DataPassing {
+
+    public LinkedHashMap<String, List<String>> setData(String name);
+}
